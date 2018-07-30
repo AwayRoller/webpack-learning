e7f7e0d275ab31edbc694cc055f6589619eba4f8
Unknown <vizi.g.zsolt@gmail.com>
2018-07-30 13:43:09 +0300

    Modules - import, export

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ Learning some basics about Webpack
 
 1. Zero Configuration Compilation
 2. Dedicated Configuration File
+3. Modules - import, export
--- src/main.js
+++ src/main.js
@@ -1,1 +1,22 @@
-alert('main');
+/*same as
+* import notify from './Notification';
+*
+* var notify = require('./Notification')
+*
+* */
+
+
+
+//import as a function
+import {notify, log} from './Notification';
+
+notify('Here is a message');
+
+log('Here is a message');
+
+
+
+/*
+import notification from './Notification';
+
+console.log(notification);*/
